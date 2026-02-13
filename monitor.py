c8a2abe5f02f9a589549a144ad3361f4778f4a43
VictoriaVilar <victoriavilar11@gmail.com>
2026-02-12 22:59:17 -0500

    able to fetch the statuses

--- monitor.py
+++ monitor.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+from urllib.parse import urlparse
+import ssl
 
 # get urls_file name from command line
 if len(sys.argv) != 2:
@@ -10,32 +12,59 @@ if len(sys.argv) != 2:
 urls_file = sys.argv[1]
 
 # server, port, and path should be parsed from url
-host = 'inet.cs.fiu.edu'
-port = 80 # use port 80 for http and port 443 for https
-path = '/'
+with open(urls_file, 'r') as f:
+    urls = f.readlines()
 
-sock = None
-# create client socket, connect to server
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-    sock.connect((host, port))
-except Exception as e:
-    print(f'Network Error:\n {e}')
+for url in urls:
+    url = url.strip()
+    if not url:
+        continue
 
-if sock:
-    # send http request
-    request = f'GET {path} HTTP/1.0\r\n'
-    request += f'Host: {host}\r\n'
-    request += '\r\n'
-    sock.send(bytes(request, 'utf-8'))
+    print(f"URL: {url}")
 
-    # receive http response
-    response = b''
-    while True:
-        data = sock.recv(4096)
-        response += data
-        if not data:
-            break
-    print(response.decode('utf-8'))
-    sock.close()
+    parsed = urlparse(url)
+
+    host = parsed.hostname
+    path = parsed.path
+
+    sock = None
+    # create client socket, connect to server
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        if parsed.scheme == 'https':
+            context = ssl.create_default_context()
+            port = 443
+            sock = context.wrap_socket(sock, server_hostname=host)
+        else:
+            port = 80
+        
+        sock.connect((host, port))
+
+        # send http request
+        request = f'GET {path} HTTP/1.0\r\n'
+        request += f'Host: {host}\r\n'
+        request += '\r\n'
+
+        sock.send(request.encode())
+
+        # receive http response
+        response = b''
+        while True:
+            data = sock.recv(4096)
+            response += data
+            if not data:
+                break
+            
+
+        response_text = response.decode('utf-8', errors='ignore')
+        status_line = response_text.split('\r\n')[0]
+        status = status_line.split(' ', 1)[1]
+
+        print(f"Status: {status}")
+
+    except Exception as e:
+        print('Status: Network Error')
+
+    if sock:
+        sock.close()
